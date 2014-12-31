06f852b71194aa65c00a2990811ee8de2c9d6f10
Seun Wright <seunaw@gmail.com>
2014-12-31 12:57:32 -0800

    new files

---
+++ lab5/src/main/java/ca/uwo/csd/cs2212/seun/BankAccount.java
@@ -0,0 +1,19 @@
+package ca.uwo.csd.cs2212.seun;
+
+public class BankAccount {
+	
+	private double balance;
+
+	public BankAccount(double balance) {
+		this.balance = balance;
+	}
+
+	public double debit(double amount) {
+		if (balance < amount) {
+			amount = balance;
+		}
+
+		balance -= amount;
+		return amount;
+	}
+}
