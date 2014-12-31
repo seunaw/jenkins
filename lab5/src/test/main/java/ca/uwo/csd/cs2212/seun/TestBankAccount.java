ecce621940e3f45cc6fe3f5db51f94c62cd1807c
Seun Wright <seunaw@gmail.com>
2014-12-31 13:49:03 -0800

    New test

---
+++ lab5/src/test/main/java/ca/uwo/csd/cs2212/seun/TestBankAccount.java
@@ -0,0 +1,17 @@
+package ca.uwo.csd.cs2212.seun;
+ 
+import junit.framework.Assert;
+import org.junit.Before;
+import org.junit.Test;
+ 
+public class TestBankAccount {
+    
+    @Test
+    public void testDebitWithSufficientFunds() {
+        BankAccount account = new BankAccount(10);
+        double amount = account.debit(5);
+
+        Assert.assertEquals(5.0, amount);
+    }
+ 
+}
